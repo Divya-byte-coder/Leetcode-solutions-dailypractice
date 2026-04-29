9cbe1731fd64e3695ed21fb5422a67f4e0f6133d
Divya-byte-coder <divyaparthiban3011@gmail.com>
2026-04-29 17:27:23 +0530

    841-Keys and Rooms.java

---
+++ 841-Keys and Rooms.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> visited = new HashSet<>();
+        dfs(0, rooms, visited);
+        return visited.size() == rooms.size();
+    }
+    private void dfs(int room, List<List<Integer>> rooms, Set<Integer> visited) {
+        if (visited.contains(room)) return;
+        visited.add(room);
+        for (int key : rooms.get(room)) {
+            dfs(key, rooms, visited);
+        }
+    }
+}
