8ac9dc379fbc8896dd293c485d6710c72f1408cf
Divya-byte-coder <divyaparthiban3011@gmail.com>
2026-06-01 06:59:51 +0530

    2144-Minimum Cost of Buying Candies With Discount.java

---
+++ 2144-Minimum Cost of Buying Candies With Discount.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int ans=0;
+        int c=0;
+        for(int i=cost.length-1;i>=0;i--){
+            c++;
+            if(c%3!=0){
+                ans+=cost[i];
+            }
+        }
+        return ans;
+    }
+}
